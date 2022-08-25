b02d0d4b6727f68bb9f3e324c42c075ec2743a57
CurvyLantern <ashfaqnasim33@gmail.com>
2022-08-25 14:30:00 +0600

    upload

---
+++ src/components/Content.vue
@@ -0,0 +1,24 @@
+
+<script setup>
+  const props = defineProps(['textColor'])
+</script>
+
+<template>
+    <div>
+      <h1>Hello World</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum, commodi?</p>
+    </div>
+</template>
+
+
+
+<style scoped>
+  div{
+    color: v-bind(props.textColor);
+  }
+  h1 {
+    font-size: calc(5vw + 30px);
+    font-weight: bold;
+    white-space: pre;
+  }
+</style>
